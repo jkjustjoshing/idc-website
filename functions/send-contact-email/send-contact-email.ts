a45aca52fe5941d08ea2663d541f60e07353101c
Josh Kramer <josh@kramer.run>
2023-02-08 00:43:03 -0500

    Forward request to Google Docs via Netlify Function

---
+++ functions/send-contact-email/send-contact-email.ts
@@ -0,0 +1,49 @@
+import type { Handler } from '@netlify/functions'
+import fetch from 'node-fetch'
+import { EXTERNAL_URL } from '@/utils/formUtils'
+
+export const handler: Handler = async (event, context) => {
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: 'Bad Request',
+    }
+  }
+
+  let formData: string
+  if (event.headers['content-type'] === 'application/json') {
+    const body = JSON.parse(event.body)
+    formData = Object.entries(body)
+      .map(([key, value]) => encodeURI(key) + '=' + encodeURI(String(value)))
+      .join('&')
+  } else if (
+    event.headers['content-type'] === 'application/x-www-form-urlencoded'
+  ) {
+    formData = event.body
+  } else {
+    return {
+      statusCode: 400,
+      body: 'Bad Request',
+    }
+  }
+
+  const response = await fetch(EXTERNAL_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: formData,
+  })
+
+  if (!response.ok) {
+    return {
+      statusCode: response.status,
+      body: response.statusText,
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: event.body,
+  }
+}
--- src/utils/formUtils.ts
+++ src/utils/formUtils.ts
@@ -4,15 +4,23 @@
 // In the future, maybe use https://www.mailgun.com/pricing/ to send an email when a form is submitted
 // using Netlify functions? https://dev.to/zicsus/send-mail-with-netlify-and-mailgun-2eoh
 
-export const FORM_URL =
+export const FORM_URL = '/.netlify/functions/send-contact-email'
+export const EXTERNAL_URL =
   'https://script.google.com/macros/s/AKfycbxwaQEwEIyQQ__S9SKi4pIKriUu1LxbpKuUMMjh-De5qNzAgSWGZhbiPkwpW3TzH4-Y/exec'
 
 export const submitForm = async (form: HTMLFormElement) => {
   const formData = new FormData(form)
-  const response = await fetch(FORM_URL, {
-    method: 'POST',
-    body: formData,
-  })
-  console.log('form', formData, response)
-  return response
+  const body: Record<string, any> = {}
+  formData.forEach((value, key) => (body[key] = value))
+
+  try {
+    const response = await fetch(FORM_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+    return { success: response }
+  } catch (e) {
+    return { error: e }
+  }
 }
