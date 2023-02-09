12b5c45c13ec43cb5ccf58ea6a854d0cae0c4b41
Josh Kramer <josh@kramer.run>
2023-02-09 00:27:59 -0500

    Require CAPTCHA for form

--- functions/send-contact-email/send-contact-email.ts
+++ functions/send-contact-email/send-contact-email.ts
@@ -3,6 +3,37 @@ import fetch from 'node-fetch'
 import { EXTERNAL_URL } from '@/utils/forms/config'
 
 // TODO Add captcha
+const verifyCaptcha = async (captcha: string) => {
+  const response = await fetch('https://hcaptcha.com/siteverify', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: [
+      `response=${captcha}`,
+      `secret=${process.env.HCAPTCHA_SECRET}`,
+      `sitekey=${process.env.HCAPTCHA_KEY}`,
+    ].join('&'),
+  })
+  if (!response.ok) {
+    throw new Error('Captcha verification failed (network error)')
+  }
+  const result = await response.json()
+
+  if (
+    result &&
+    typeof result === 'object' &&
+    'success' in result &&
+    !result.success
+  ) {
+    console.log(result)
+    throw new Error(
+      `Captcha verification failed (invalid captcha${
+        'error-codes' in result ? ' - ' + result['error-codes'] : ''
+      })`
+    )
+  }
+}
 
 export const handler: Handler = async (event, context) => {
   if (!event.body) {
@@ -12,29 +43,38 @@ export const handler: Handler = async (event, context) => {
     }
   }
 
-  let formData: string
+  let body: any
   if (event.headers['content-type'] === 'application/json') {
-    const body = JSON.parse(event.body)
-
-    // For debug purposes
-    body['_no_email'] = true
-
-    formData = Object.entries(body)
-      .map(
-        ([key, value]) =>
-          encodeURIComponent(key) + '=' + encodeURIComponent(String(value))
-      )
-      .join('&')
+    body = JSON.parse(event.body)
   } else if (
     event.headers['content-type'] === 'application/x-www-form-urlencoded'
   ) {
-    formData = event.body
+    body = Object.fromEntries(
+      event.body.split('&').map((pair) => {
+        const [key, value] = pair.split('=')
+        return [key, decodeURIComponent(value)]
+      })
+    )
   } else {
     return {
       statusCode: 400,
       body: 'Bad Request',
     }
   }
+
+  // For debug purposes
+  body['_no_email'] = true
+
+  const captcha = body['h-captcha-response']
+  await verifyCaptcha(captcha)
+  delete body['h-captcha-response']
+
+  const formData = Object.entries(body)
+    .map(
+      ([key, value]) =>
+        encodeURIComponent(key) + '=' + encodeURIComponent(String(value))
+    )
+    .join('&')
 
   const response = await fetch(EXTERNAL_URL, {
     method: 'POST',
